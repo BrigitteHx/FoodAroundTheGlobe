f8ae5c6a2b57d53b13c5a69d23157d7d2e591ab3
Jill Lukassen <JillLukassen@hotmail.com>
2025-12-08 19:52:54 +0100

    change

--- FoodAroundTheGlobe/FoodAroundTheGlobe/Models/Workshop.cs
+++ FoodAroundTheGlobe/FoodAroundTheGlobe/Models/Workshop.cs
@@ -2,10 +2,14 @@
 {
     public class Workshop
     {
-            public int Id { get; set; }
-            public string Titel { get; set; }
-            public string Beschrijving { get; set; }
-            public DateTime Datum { get; set; } = DateTime.Now;
-            public int MaxAantalDeelnemers { get; set; } = 0;
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public int MaxParticipants { get; set; }
+        public int Participants { get; set; }
+
     }
 }
+
+
