649c96bf152bd53c082f7166ce17607c7bad888b
Eitjemee <aimeevos04@gmail.com>
2025-12-20 19:12:44 +0100

    Homepagina toegevoegd als .htmlcs en .cs. Titel van de index pagina veranderd van 'Home' naar 'Index' Homepagina toegevoegd aan de navigatie.

---
+++ FoodAroundTheGlobe/FoodAroundTheGlobe/Pages/Shared/Homepagina.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodAroundTheGlobe.Pages.Shared
+{
+    public class HomepaginaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
+
