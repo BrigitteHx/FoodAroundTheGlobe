9b444e8f3112845924f387e4b0bf443fd34f7e98
Jill Lukassen <JillLukassen@hotmail.com>
2025-12-22 11:26:35 +0100

    Contactpagina en plattegrond aangepast. Ook de Pages goed geplaatst. Niet meer in Shared, anders zijn ze niet te openen.

---
+++ FoodAroundTheGlobe/FoodAroundTheGlobe/Pages/Workshop.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FoodAroundTheGlobe.Data;
+using FoodAroundTheGlobe.Models;
+
+public class WorkshopPageModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+    public List<Workshops> Workshops { get; set; }
+
+    public WorkshopPageModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public void OnGet()
+    {
+        Workshops = _context.Workshops.ToList();
+    }
+}
+
+
+
